cc3722942741333af8d5454b6a25f39120288165
VN23js <90689988+VN23js@users.noreply.github.com>
2026-07-20 16:12:42 +0300

    Исправили импорт

--- my-react-app/src/components/LeftPanel.tsx
+++ my-react-app/src/components/LeftPanel.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import type { AppDispatch, RootState } from '../redux/store.js';
-import { getAllSessionFetch } from '../redux/ProfileSlice.js';
+import type { AppDispatch, RootState } from '../redux/store.ts';
+import { getAllSessionFetch } from '../redux/ProfileSlice.ts';
 
 export default function LeftPanel() {
   const DataWinIndex = useSelector(
